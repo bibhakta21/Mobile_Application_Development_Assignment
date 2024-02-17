4a0a5518ee9c8bdb562846e707ef4c8872e608ce
bibhakta21 <bibhaktalamsal8@gmail.com>
2024-02-17 20:12:36 +0545

    header for book details page added.

---
+++ Hamrobook Nepal/lib/Pages/BookDetails/HeaderWidget.dart
@@ -0,0 +1,90 @@
+import 'package:e_book/Components/BackButton.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class BookDetailsHeader extends StatelessWidget {
+  final String coverUrl;
+  final String title;
+  final String author;
+  final String description;
+  final String rating;
+  final String pages;
+  final String langugae;
+  final String audioLen;
+  const BookDetailsHeader(
+      {super.key,
+      required this.coverUrl,
+      required this.title,
+      required this.author,
+      required this.description,
+      required this.rating,
+      required this.pages,
+      required this.langugae,
+      required this.audioLen});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(height: 50),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            MyBackButton(),
+            SvgPicture.asset(
+              "Assets/Icons/heart.svg",
+              color: Theme.of(context).colorScheme.background,
+            ),
+          ],
+        ),
+        SizedBox(height: 40),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.network(
+                coverUrl,
+                width: 170,
+              ),
+            )
+          ],
+        ),
+        SizedBox(height: 30),
+        Text(
+          title,
+          maxLines: 1,
+          textAlign: TextAlign.center,
+          style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+                color: Theme.of(context).colorScheme.background,
+              ),
+        ),
+        Text(
+          "Author : $author",
+          style: Theme.of(context).textTheme.labelMedium?.copyWith(
+                color: Theme.of(context).colorScheme.onPrimaryContainer,
+              ),
+        ),
+        SizedBox(height: 10),
+        Text(
+          description,
+          maxLines: 2,
+          textAlign: TextAlign.center,
+          style: Theme.of(context).textTheme.labelSmall?.copyWith(
+                color: Theme.of(context).colorScheme.onPrimaryContainer,
+              ),
+        ),
+        SizedBox(height: 20),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+           
+           
+            
+           
+          ],
+        )
+      ],
+    );
+  }
+}

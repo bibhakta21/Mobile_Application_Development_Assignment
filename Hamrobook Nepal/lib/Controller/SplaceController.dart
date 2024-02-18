d8d5238b66b4c1b1134206d9ea9b2d54cd887e20
bibhakta21 <bibhaktalamsal8@gmail.com>
2024-02-18 11:03:36 +0545

    firebase authentication and getx state management done

---
+++ Hamrobook Nepal/lib/Controller/SplaceController.dart
@@ -0,0 +1,15 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get/get.dart';
+
+class SplaceController extends GetxController {
+  final auth = FirebaseAuth.instance;
+
+  @override
+  void onInit() {
+   
+  }
+
+  void splaceController() {
+  
+  }
+}

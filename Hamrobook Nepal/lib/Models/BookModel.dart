6079a107cba50431751f571211e0733d868f17d8
bibhakta21 <bibhaktalamsal8@gmail.com>
2024-02-19 19:16:36 +0545

    added necessary field for book

---
+++ Hamrobook Nepal/lib/Models/BookModel.dart
@@ -0,0 +1,18 @@
+class BookModel {
+  String? id;
+  String? title;
+  String? description;
+  String? rating;
+  int? pages;
+  String? language;
+  String? audioLen;
+  String? author;
+  String? aboutAuthor;
+  String? bookurl;
+  String? audioUrl;
+  String? category;
+  String? coverUrl;
+  int? price;
+  String? numberofRating;
+  String? genre; // New field for genre
+}

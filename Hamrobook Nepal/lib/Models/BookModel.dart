058d85863693510659cd2565ddbd4b7c199c080a
bibhakta21 <bibhaktalamsal8@gmail.com>
2024-02-19 19:19:48 +0545

    conversion of an object into a JSON representation

--- Hamrobook Nepal/lib/Models/BookModel.dart
+++ Hamrobook Nepal/lib/Models/BookModel.dart
@@ -35,4 +35,76 @@ class BookModel {
     this.genre, // Initialize genre in the constructor
   });
 
+  BookModel.fromJson(Map<String, dynamic> json) {
+    if (json["id"] is String) {
+      id = json["id"];
+    }
+    if (json["title"] is String) {
+      title = json["title"];
+    }
+    if (json["description"] is String) {
+      description = json["description"];
+    }
+    if (json["rating"] is String) {
+      rating = json["rating"];
+    }
+    if (json["pages"] is int) {
+      pages = json["pages"];
+    }
+    if (json["language"] is String) {
+      language = json["language"];
+    }
+    if (json["audioLen"] is String) {
+      audioLen = json["audioLen"];
+    }
+    if (json["author"] is String) {
+      author = json["author"];
+    }
+    if (json["aboutAuthor"] is String) {
+      aboutAuthor = json["aboutAuthor"];
+    }
+    if (json["bookurl"] is String) {
+      bookurl = json["bookurl"];
+    }
+    if (json["audioUrl"] is String) {
+      audioUrl = json["audioUrl"];
+    }
+    if (json["category"] is String) {
+      category = json["category"];
+    }
+    if (json["coverUrl"] is String) {
+      coverUrl = json["coverUrl"];
+    }
+    if (json["price"] is int) {
+      price = json["price"];
+    }
+    if (json["numberofRating"] is int) {
+      numberofRating = json["numberofRating"];
+    }
+    if (json["genre"] is String) {
+      genre = json["genre"];
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> _data = <String, dynamic>{};
+    _data["id"] = id;
+    _data["title"] = title;
+    _data["description"] = description;
+    _data["rating"] = rating;
+    _data["pages"] = pages;
+    _data["language"] = language;
+    _data["audioLen"] = audioLen;
+    _data["author"] = author;
+    _data["aboutAuthor"] = aboutAuthor;
+    _data["bookurl"] = bookurl;
+    _data["audioUrl"] = audioUrl;
+    _data["category"] = category;
+    _data["coverUrl"] = coverUrl;
+    _data["price"] = price;
+    _data["numberofRating"] = numberofRating;
+    _data["genre"] = genre;
+    return _data;
+  }
+
 }

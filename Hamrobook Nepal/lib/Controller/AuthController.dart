e43699335d48957531c012d1dcc1a1a222ad778f
bibhakta21 <bibhaktalamsal8@gmail.com>
2024-02-21 10:14:05 +0545

    home page and welcome page imported

--- Hamrobook Nepal/lib/Controller/AuthController.dart
+++ Hamrobook Nepal/lib/Controller/AuthController.dart
@@ -1,5 +1,6 @@
 import 'package:e_book/Config/Messages.dart';
-
+import 'package:e_book/Pages/Homepage/HomePage.dart';
+import 'package:e_book/Pages/WelcomePage/WelcomePage.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
 import 'package:google_sign_in/google_sign_in.dart';

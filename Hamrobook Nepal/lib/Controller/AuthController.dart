d4320627515c2069c13cd07e2d214623165e913e
bibhakta21 <bibhaktalamsal8@gmail.com>
2024-02-21 10:13:49 +0545

    firebase inctance called

---
+++ Hamrobook Nepal/lib/Controller/AuthController.dart
@@ -0,0 +1,15 @@
+import 'package:e_book/Config/Messages.dart';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get/get.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class AuthController extends GetxController {
+  RxBool isLoading = false.obs;
+
+  final auth = FirebaseAuth.instance;
+
+
+
+
+}
